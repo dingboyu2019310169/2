a5921013c5fdbafa2d71c8a943ff2d96328205e9
dingboyu2019310169 <72386139+dingboyu2019310169@users.noreply.github.com>
2020-11-08 18:11:33 +0800

    Add files via upload

---
+++ StudentInterface.java
@@ -0,0 +1,5 @@
+package Two;
+public interface StudentInterface {
+    public abstract void setFee(int fee);
+    public abstract void getFee(int fee);
+}
---
+++ TeacherInterface.java
@@ -0,0 +1,5 @@
+package Two;
+public interface TeacherInterface {
+    public abstract void setPay(int pay);
+    public abstract void getPay(int pay);
+}
